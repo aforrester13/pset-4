ec9cddfbd3824256488b46266a106ecb9dea60d8
Aidan Forrester <AForrester@ucvts.tec.nj.us>
2019-11-04 13:12:45 -0500

    a commit

--- src/sum.js
+++ src/sum.js
@@ -1,1 +1,10 @@
-const readlineSync = require("readlineSync");
+const readlineSync = require("readline-sync");
+
+let x = -1;
+do {
+    x = Number(readlineSync.question("\nEnter first integer: "));
+    y = Number(readlineSync.question("Enter second integer: "))
+} while (x < y) {
+    theSum = y + (x++)
+    (x++)
+}
