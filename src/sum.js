94aa37f88459b7b00f2a70798588d0bcdad527c2
Aidan Forrester <AForrester@ucvts.tec.nj.us>
2019-11-12 13:22:48 -0500

    next commit

--- src/sum.js
+++ src/sum.js
@@ -1,10 +1,25 @@
 const readlineSync = require("readline-sync");
 
-let x = -1;
+let x = 0;
+let y = 0;
+let z = 0;
+
+const lowerBound = readlineSync.question("\nLowerbound: ")
+const upperBound = readlineSync.question("Upperbound: ")
+
 do {
-    x = Number(readlineSync.question("\nEnter first integer: "));
-    y = Number(readlineSync.question("Enter second integer: "))
-} while (x < y) {
-    theSum = y + (x++)
-    (x++)
+    x = readlineSync.question("Lowerbound: ")
+    y = readlineSync.question("Upperbound: ")
+} while (x > upperBound || x < Number.MIN_SAFE_INTEGER || x > Number.MAX_SAFE_INTEGER || y < Number.MIN_SAFE_INTEGER || y > Number.MAX_SAFE_INTEGER);
+
+while (x < y) {
+
+    if (x % 2 === 0) {
+      z = x + x
+    } else {
+
+    }
+  x++
 }
+
+console.log("\n" + z)
