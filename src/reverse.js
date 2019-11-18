1aae7143613b3b3e7661d0c971c3e2c21b148d1f
Aidan Forrester <Aidan@Daras-MacBook-Air.local>
2019-11-17 19:56:47 -0500

    final commit

--- src/reverse.js
+++ src/reverse.js
@@ -1,8 +1,25 @@
 const readlineSync = require("readline-sync");
 
-let x = 0;
+let theInteger = 0;
+let space = "";
 
-console.log("")
+console.log("");
+
 do {
-     integer = Number(readlineSync.question("Positive integer: "));
-} while (integer < 1 || integer > Number.MAX_SAFE_INTEGER || Number.isNaN(integer) || !Number.isInteger(integer));
+      theInteger = Number(readlineSync.question("Positive integer: "));
+} while (theInteger < 1 || theInteger > Number.MAX_SAFE_INTEGER || Number.isNaN(theInteger) || !Number.isInteger(theInteger));
+
+while (theInteger > 0) {
+
+  let value = (theInteger % 10)
+  theInteger = Math.floor(theInteger / 10)
+
+  if (theInteger > 0) {
+    space = space + value + ", ";
+  } else {
+    space = space + value + ". ";
+  }
+
+}
+
+console.log("\n" + space + "\n");
