1b5b5c203fa5be35b28ef255b8c685303391bdb6
Aidan Forrester <AForrester@ucvts.tec.nj.us>
2019-11-22 13:23:12 -0500

    second commit

--- src/credit.js
+++ src/credit.js
@@ -7,6 +7,7 @@ let value1 = 0;
 let sum = 0;
 let value2 = 0;
 let value3 = 0;
+let value4 = 0;
 
 console.log()
 
@@ -19,16 +20,22 @@ while (creditCard > 0) {
     if (creditCard > 0) {
        value = Math.floor((creditCard % 100) / 10);
        creditCard = Math.floor(creditCard / 100);
-       value = value * 2;
+       value4 = value * 2;
 
-        if (value >= 10) {
-         value1 = value / 10;
+       if (value4 < 10) {
+         console.log(value4);
+         sum = sum + value4
+       }
+
+       if (value4 >= 10) {
+         console.log(value4);
+         value1 = value4 / 10;
          value2 = Math.floor(value1);
-         value3 = value % 10;
-         sum = sum + value2 + value3
-        }
-
-       sum = sum + value
+         value3 = value4 % 10;
+         sum = sum + value2 + value3;
+         console.log(value2);
+         console.log(value3);
+       }
     }
 }
 console.log("\n" + sum);
