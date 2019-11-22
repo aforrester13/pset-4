92f53c4cfb541174f15e33c11b21a0d915e87490
aforrester13 <55455905+aforrester13@users.noreply.github.com>
2019-11-22 08:50:39 -0500

    Create luigi.js

---
+++ src/luigi.js
@@ -0,0 +1,37 @@
+Learn more or give us feedback
+const readlineSync = require("readline-sync");
+
+var theHeight = 0;
+var space = "##";
+
+console.log("");
+
+do {
+      theHeight = Number(readlineSync.question("Height: "));
+} while (theHeight < 1 || theHeight > 24 || Number.isNaN(theHeight) || !Number.isInteger(theHeight));
+
+var theHeight2 = theHeight;
+var theHeight3 = theHeight;
+
+while (theHeight2 > 1) {
+      space = " " + space
+    --theHeight2
+}
+
+while (theHeight3 > 1) {
+      opposite = opposite + " "
+      --theHeight3
+}
+
+console.log("\n" + space + opposite)
+
+while (theHeight > 1) {
+    var space1 = space.replace(" #", "##")
+    var opposite1 = space.replace("# ", "##")
+    console.log(space1)
+    console.log(opposite)
+    --theHeight
+    var space = space1
+    var opposite = opposite1
+}
+console.log()
